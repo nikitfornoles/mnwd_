f83ee79d431d10816621834709d0648f56c6d238
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2017-10-03 00:28:17 +0800

    refactor notification manager php file

--- mobile_notification_manager.php
+++ mobile_notification_manager.php
@@ -6,26 +6,63 @@
 		$session_accountid = test_input($_POST['accountid']);
 		$userid = test_input($_POST['userid']);
 
-		$result = array();
+		//$session_accountid = 1;
+		//$userid = 2;
+
+		$r = array();
 
 		require_once('mobile_notification_accountactivation.php');
 		list ($total_account, $total_activated_account, $accountid, $accountno) = getAccountStats($userid, $dbconn);
 
+		$non_activated_account = -1;
+
 		if (($total_account > 1) && ($total_account != $total_activated_account)) {
 			$non_activated_account = $total_account - $total_activated_account;
-			$notif_account_activation = "You have $non_activated_account non activated accounts.";
-			array_push(
-				$result,
-				array(
-					"notif_account_activation"
-				)
-			);
 		}
 
-		
+		$sql = "SELECT * FROM `reading` WHERE `accountid` = $session_accountid ORDER BY `billingdate` DESC LIMIT 1";
+		$result = mysqli_query($dbconn, $sql);
+		$row = mysqli_fetch_array($result);
+
+		$billingdate  = $row['billingdate'];
+		$bill = $row['bill'];
+		$duedate = $row['duedate'];
+		$disconnection_date = $row['disconnection_date'];
+
+		$sql = "SELECT CURDATE()";
+		$result = mysqli_query($dbconn, $sql);
+		$row = mysqli_fetch_array($result);
+		$curr_date = $row[0];
+
+		$due_date_countdown = -1;
+		if ($curr_date <= $duedate) {
+			$sql = "SELECT DATEDIFF('$duedate', '$curr_date')";
+			$result = mysqli_query($dbconn, $sql);
+			$row = mysqli_fetch_array($result);
+			$due_date_countdown = $row[0];
+		}
+
+		$disconnection_date_countdown = -1;
+		if ($curr_date <= $disconnection_date) {
+			$sql = "SELECT DATEDIFF('$disconnection_date', '$curr_date')";
+			$result = mysqli_query($dbconn, $sql);
+			$row = mysqli_fetch_array($result);
+			$disconnection_date_countdown = $row[0];
+		}
+
+		array_push(
+			$r,
+			array (
+				"notif1" => $non_activated_account,
+				"notif2" => $due_date_countdown,
+				"notif3" => $disconnection_date_countdown
+			)
+		);
+
+		echo json_encode(array('result'=>$r));
 	}
-	else {
-		echo "<center><h1>Illegal access detected!<h1></center>";
+	else { 
+		echo "<center><h1>Illegal access detected!<h1></center>"; 
 	}
 	mysqli_close($dbconn);
 ?>
